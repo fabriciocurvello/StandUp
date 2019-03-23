6c961bb18cd6a6600e2bb09f28e49aa1ac4ea231
Fabricio Curvello <tiofab@gmail.com>
2019-03-23 16:54:14 -0300

    Atividade 4 (Tarefa 2) concluida

--- app/src/main/java/com/example/standup/MainActivity.java
+++ app/src/main/java/com/example/standup/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.standup;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Toast;
@@ -9,10 +17,17 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NotificationManager mNotificationManager;
+
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
         alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -20,13 +35,52 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 String toastMessage;
                 if (isChecked) {
+
+                    deliverNotification(MainActivity.this);
                     toastMessage = getString(R.string.Toast_Alarm_On);
+
                 } else {
+
+                    mNotificationManager.cancelAll();
                     toastMessage = getString(R.string.Toast_Alarm_Off);
                 }
 
                 Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
             }
         });
+
+        createNotificationChannel();
+    }
+
+    //Canal de notificações é necessário para Android 8 e superiores
+    public void createNotificationChannel() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        //Verificar a versão do Android, pois canal de notificação é válido apenas para versões 8 ou superiores
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "Stand Up notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription(getString(R.string.descricao_notificacao));
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent conttentPendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(getString(R.string.alerta_titulo))
+                .setContentText(getString(R.string.alerta_texto))
+                .setContentIntent(conttentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
