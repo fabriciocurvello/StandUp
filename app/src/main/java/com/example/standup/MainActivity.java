53eaa85d2cb9253480a7c67915acbc09ab44e8d3
Fabricio Curvello <tiofab@gmail.com>
2019-03-23 15:10:57 -0300

    Atividade 3 concluida

--- app/src/main/java/com/example/standup/MainActivity.java
+++ app/src/main/java/com/example/standup/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.standup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String toastMessage;
+                if (isChecked) {
+                    toastMessage = getString(R.string.Toast_Alarm_On);
+                } else {
+                    toastMessage = getString(R.string.Toast_Alarm_Off);
+                }
+
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
